deeef74e93d71a3baeed558183c2afcefb131ee7
Neill Alexander <neillalexander@gmail.com>
2011-08-13 21:40:39 +0100

    Better version of echo now builds the args string from a map

--- src/rtm_clj/api.clj
+++ src/rtm_clj/api.clj
@@ -8,28 +8,32 @@
 (def *api-key* (atom ""))
 (def *shared-secret* (atom ""))
 
-
 (defn set-api-key!
   "Sets the key for the session"
   [key]
   (reset! *api-key* key))
-
 
 (defn set-shared-secret!
   "Sets the shared secret for the session"
   [secret]
   (reset! *shared-secret* secret))
 
-
 (defn build-rtm-url
   "Builds the url to hit the rest service"
   [method]
   (str *base-url* "method=" method "&api_key=" @*api-key*))
 
+(defn- call-api
+  [method param-map])
+
+(defn- build-params
+  "Builds key=value&key=value&key=value to append onto url"
+  [param-map]
+  (apply str (drop-last (interleave (map #(str (first %) "=" (second %)) m) (repeat "&")))))
 
 ;; see http://www.rememberthemilk.com/services/api/request.rest.rtm
 (defn echo
   "Calls the rtm.test.echo method with the specified params"
-  [params]
-  (let [url (str (build-rtm-url "rtm.test.echo") "&" params)]
+  [param-map]
+  (let [url (str (build-rtm-url "rtm.test.echo") "&" (build-params param-map))]
     (http/get url)))
